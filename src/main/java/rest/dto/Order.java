4ecfff552b35875fc89898dc9d8d7d274f79c95c
Anna <anna.ostaptsuk1@gmail.com>
2023-01-09 20:40:44 +0200

    rest

--- src/main/java/rest/dto/Order.java
+++ src/main/java/rest/dto/Order.java
@@ -2,62 +2,71 @@ package rest.dto;
 
 public class Order { //Создайте dto объект для POST запроса /test-orders
 
-    private String status;
-    private Long courierId;
-    private String customerName;
-    private String customerPhone;
-    private String comment;
-    private Long id;
 
+        private String status;
+        private Long courierId;
+        private String customerName;
+        private String customerPhone;
+        private String comment;
+        private Long id;
 
+        public Order() {
+        }
 
-    public Long getCourierId() {
-        return courierId;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public String getCustomerPhone() {
-        return customerPhone;
-    }
-
-    public String getComment() {
-        return comment;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setComment(String comment) {
+    public Order(String status, Long courierId, String customerName, String customerPhone, String comment) {
+        this.status = status;
+        this.courierId = courierId;
+        this.customerName = customerName;
+        this.customerPhone = customerPhone;
         this.comment = comment;
     }
 
-    public void setCustomerPhone(String customerPhone) {
-        this.customerPhone = customerPhone;
+
+        public String getStatus() {
+            return status;
+        }
+
+        public void setStatus(String status) {
+            this.status = status;
+        }
+
+        public Long getCourierId() {
+            return courierId;
+        }
+
+        public void setCourierId(Long courierId) {
+            this.courierId = courierId;
+        }
+
+        public String getCustomerName() {
+            return customerName;
+        }
+
+        public void setCustomerName(String customerName) {
+            this.customerName = customerName;
+        }
+
+        public String getCustomerPhone() {
+            return customerPhone;
+        }
+
+        public void setCustomerPhone(String customerPhone) {
+            this.customerPhone = customerPhone;
+        }
+
+        public String getComment() {
+            return comment;
+        }
+
+        public void setComment(String comment) {
+            this.comment = comment;
+        }
+
+        public Long getId() {
+            return id;
+        }
+
+        public void setId(Long id) {
+            this.id = id;
+        }
     }
-
-    public void setCustomerName(String customerName) {
-        this.customerName = customerName;
-    }
-
-    public void setCourierId(Long courierId) {
-        this.courierId = courierId;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-
-}
