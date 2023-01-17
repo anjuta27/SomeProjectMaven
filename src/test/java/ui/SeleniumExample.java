858da47df93d4c3828b293c13cdedea21ac4f0a9
Anna <anna.ostaptsuk1@gmail.com>
2023-01-17 19:06:58 +0200

    Selenide

---
+++ src/test/java/ui/SeleniumExample.java
@@ -0,0 +1,29 @@
+package ui;
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+
+
+     public class SeleniumExample {
+         @Test
+         public void testGoogleSearch() throws InterruptedException {
+        // Optional. If not specified, WebDriver searches the PATH for chromedriver.
+        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("http://www.google.com/");
+        Thread.sleep(5000);  // Let the user actually see something!
+        WebElement searchBox = driver.findElement(By.xpath("//*[@name='q']"));
+        searchBox.sendKeys("ChromeDriver");
+        searchBox.submit();
+        Thread.sleep(5000);  // Let the user actually see something!
+        driver.quit();
+    }
+}
+
